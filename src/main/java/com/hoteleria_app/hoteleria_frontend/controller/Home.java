c900be359abd40db97d6e4e8251a6fc8ed9a18ee
Juan Chuc <dev.juan.eloper@gmail.com>
2024-10-08 22:04:35 -0600

    new changes

--- src/main/java/com/hoteleria_app/hoteleria_frontend/controller/Home.java
+++ src/main/java/com/hoteleria_app/hoteleria_frontend/controller/Home.java
@@ -3,6 +3,7 @@ package com.hoteleria_app.hoteleria_frontend.controller;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -16,13 +17,14 @@ public class Home {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
         String url_api = "http://localhost:9090/api/hotel/getAllHotels";
 
         try {
             HttpEntity<String> requestEntity = new HttpEntity<>(null);
 
             ResponseEntity<String> response = restTemplate.postForEntity(url_api, requestEntity, String.class);
+            model.addAllAttributes(response.getHeaders());
             String responseData = response.getBody();
             System.out.println("Response from API: " + responseData);
         } catch (RestClientException e) {
