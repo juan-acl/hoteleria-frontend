efee7bc9789f8611d5336b376f80b63deb8086f1
Juan Chuc <dev.juan.eloper@gmail.com>
2024-10-13 20:45:44 -0600

    cleaning

--- src/main/java/com/hoteleria_app/hoteleria_frontend/controller/auth/signIn/SignInController.java
+++ src/main/java/com/hoteleria_app/hoteleria_frontend/controller/auth/signIn/SignInController.java
@@ -3,7 +3,6 @@ package com.hoteleria_app.hoteleria_frontend.controller.auth.signIn;
 import com.hoteleria_app.hoteleria_frontend.dto.auth.SignInDto;
 import com.hoteleria_app.hoteleria_frontend.service.auth.AuthService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
