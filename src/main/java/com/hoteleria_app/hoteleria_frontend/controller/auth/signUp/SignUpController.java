a0d9b2ab343f95bef2135b15f9750019f294c5cf
Juan Chuc <dev.juan.eloper@gmail.com>
2024-10-15 22:25:58 -0600

    register component

--- src/main/java/com/hoteleria_app/hoteleria_frontend/controller/auth/signUp/SignUpController.java
+++ src/main/java/com/hoteleria_app/hoteleria_frontend/controller/auth/signUp/SignUpController.java
@@ -1,4 +1,18 @@
 package com.hoteleria_app.hoteleria_frontend.controller.auth.signUp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class SignUpController {
+    @GetMapping("/signUp")
+    public String signUpComponent() {
+        return "signUp";
+    }
+
+    @PostMapping("/signUp")
+    public String signUp() {
+        return "signUp";
+    }
 }
