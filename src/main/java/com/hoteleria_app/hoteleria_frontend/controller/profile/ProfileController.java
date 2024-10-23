492871809acc0812d19b0f139e212b89657b4511
Juan Chuc <dev.juan.eloper@gmail.com>
2024-10-22 20:36:14 -0600

    userProfile view

--- src/main/java/com/hoteleria_app/hoteleria_frontend/controller/profile/ProfileController.java
+++ src/main/java/com/hoteleria_app/hoteleria_frontend/controller/profile/ProfileController.java
@@ -1,14 +1,75 @@
 package com.hoteleria_app.hoteleria_frontend.controller.profile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hoteleria_app.hoteleria_frontend.dto.auth.UserProfileDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class ProfileController {
+    @Value("${hotel.service.backend.url}")
+    public String BASE_URL;
+    public final String CURRENT_PATH = "/user/me";
+    public final RestTemplate restTemplate;
+    private final HttpServletRequest request;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public ProfileController(HttpServletRequest request,
+                             RestTemplate restTemplate) {
+        this.request = request;
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/me")
     public String profile(Model model) {
+        String token = (String) request.getSession().getAttribute("token");
+        if (token == null) {
+            return "redirect:/signIn";
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<String> response =
+                    restTemplate.exchange(BASE_URL + CURRENT_PATH,
+                            HttpMethod.GET,
+                            entity,
+                            String.class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                UserProfileDto userProfileDto =
+                        objectMapper.readValue(response.getBody(),
+                                UserProfileDto.class);
+                System.out.println("Response:sasdasdadad " + userProfileDto.getCurrentUser());
+                model.addAttribute("userProfile",
+                        userProfileDto.getCurrentUser());
+            } else {
+                model.addAttribute("error", "Error fetching user profile");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            model.addAttribute("error", "An error occurred: " + e.getMessage());
+        }
+
         model.addAttribute("pageContent", "me");
         return "layout";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        request.getSession().removeAttribute("token");
+        return "redirect:/signIn";
     }
 }
---
+++ src/main/java/com/hoteleria_app/hoteleria_frontend/dto/auth/UserProfileDto.java
@@ -0,0 +1,45 @@
+package com.hoteleria_app.hoteleria_frontend.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileDto {
+    private String status;
+    private String message;
+    private int count;
+    private CurrentUserDto currentUser;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CurrentUserDto {
+        private int id_user;
+        private String name;
+        private String lastname;
+        private int active;
+        private String email;
+        private String phone;
+        private String password;
+        private List<String> accesses; // Assuming accesses is a list of strings
+        private List<String> permissions; // Assuming permissions is a list
+        // of strings
+        private List<String> reservations; // Assuming reservations is a list
+        // of strings
+        private String username;
+        private List<String> authorities; // Assuming authorities is a list
+        // of strings
+        private boolean enabled;
+        private boolean accountNonExpired;
+        private boolean accountNonLocked;
+        private boolean credentialsNonExpired;
+    }
+}
